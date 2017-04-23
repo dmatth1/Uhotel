1ca1e0a8bdade8e7678cd9e5500b396ec572c18a
5530u39 <5530u39@georgia.eng.utah.edu>
2017-04-22 18:05:02 -0600

    first commit

---
+++ WEB-INF/classes/cs5530/User.java
@@ -0,0 +1,56 @@
+package cs5530;
+
+import java.sql.*;
+//import javax.servlet.http.*;
+
+public class User{
+    //Connection con;
+    public User(/*Connection con*/) {
+	    //this.con = con;
+	}
+
+    public static String register(String login, String firstname, String lastname, String password, String telnumber, Connection con) throws Exception{
+	    /*		String query;
+		String resultstr="";
+		ResultSet results; 
+		query="Select * from orders where "+attrName+"='"+attrValue+"'";
+		try{
+			results = stmt.executeQuery(query);
+        } catch(Exception e) {
+			System.err.println("Unable to execute query:"+query+"\n");
+	                System.err.println(e.getMessage());
+			throw(e);
+		}
+		System.out.println("Order:getOrders query="+query+"\n");
+		while (results.next()){
+			resultstr += "<b>"+results.getString("login")+"</b> purchased "+results.getInt("quantity") +
+							" copies of &nbsp'<i>"+results.getString("title")+"'</i><BR>\n";	
+		}
+		return resultstr;
+	    */
+
+	    String query = " insert into users (login, firstname, lastname, password, telnumber)"
+		+ " values (?, ?, ?, ?, ?)";
+	    
+	    PreparedStatement preparedStmt = con.prepareStatement(query);
+
+	    preparedStmt.setString(1, login);
+	    preparedStmt.setString(2, firstname);
+	    preparedStmt.setString(3, lastname);
+	    preparedStmt.setString(4, password);
+	    preparedStmt.setInt(5, Integer.parseInt(telnumber));
+	    
+	    try{
+		preparedStmt.executeUpdate();
+	    }
+	    
+	    catch(Exception e){
+		return e.toString();
+	        //return "Unable to register at this time.";
+	    }
+	    
+
+	    return "Successfully registered as " + login + ". Thank you " + firstname + ".";
+	    
+	}
+}
